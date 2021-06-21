f691b0d21d5bff7b65c9dfcf80ac9d7a97178b19
Aditi Atul Vaidya <61463717+AditiTheCoder@users.noreply.github.com>
2021-06-21 23:32:32 +0530

    Add files via upload

---
+++ Components/HomeScreen.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Background from "./assets/images/vinayak-chetti-main-file.jpg";
+
+export const HomeScreen = () => {
+    return (
+        <div>
+            <h1>HomeScreen</h1>
+            <div className="app__itemsContainer">
+                <Item
+                backgroundImg={Background}
+                />
+            </div>
+        </div>
+    )
+}
+
+export default HomeScreen;
