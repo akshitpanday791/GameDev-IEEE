23010fe3ec06ba9e7eeb1eb3d9a059c89c407f2d
Aditi Atul Vaidya <61463717+AditiTheCoder@users.noreply.github.com>
2021-06-21 23:35:27 +0530

    Update HomeScreen.jsx

--- src/Components/HomeScreen.jsx
+++ src/Components/HomeScreen.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import Background from "./assets/images/vinayak-chetti-main-file.jpg";
 
 export const HomeScreen = () => {
     return (
         <div>
             <h1>HomeScreen</h1>
+            <div className="app__itemsContainer">
+                <Item
+                backgroundImg={Background}
+                />
+            </div>
         </div>
     )
 }
