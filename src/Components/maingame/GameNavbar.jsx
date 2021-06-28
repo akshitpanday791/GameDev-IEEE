52c95851493e382c1eed58c8c22b9656574dcb79
Aditi Atul Vaidya <61463717+AditiTheCoder@users.noreply.github.com>
2021-06-28 14:34:10 +0530

    Update GameNavbar.jsx

--- src/Components/maingame/GameNavbar.jsx
+++ src/Components/maingame/GameNavbar.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
 import { Navbar,Container,Nav,Button } from 'react-bootstrap'
+import './GameNavbar.css'
 
 const GameNavbar = ({roomCreatedby, currentUser, exitFunction}) => {
     const [copyStatus, setCopyStatus] = React.useState("Click link to Copy");
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
-                <Navbar.Brand href="#home">Bingo Minds</Navbar.Brand>
+                <Navbar.Brand href="#home" className="navbar-brand">Bingo Minds</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav>
-                        <Nav.Link active>
+                        <Nav.Link active className="user" >
                              Room Created By : <b>{roomCreatedby}</b><br/>
                              Current User : <b>{currentUser}</b>
                         </Nav.Link>
-                        <Nav.Link active onClick={()=>{navigator.clipboard.writeText(window.location.href); setCopyStatus("Copied!")}}>
+                        <Nav.Link active onClick={()=>{navigator.clipboard.writeText(window.location.href); setCopyStatus("Copied!")}} className="link">
                             JoinUrl : <b>{window.location.href}</b><br/> <span>({copyStatus})</span>
                         </Nav.Link>
                         <Nav.Link >
-                            <Button variant="danger" onClick={exitFunction}>Exit</Button> <br/><br/>
+                            <Button variant="danger" onClick={exitFunction} className="exit">Exit</Button> <br/><br/>
                         </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
